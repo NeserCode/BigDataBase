697296707a07f02c07bc005343fa611a9625b4c5
NeserCode <73767966+NeserCode@users.noreply.github.com>
2021-10-10 00:40:29 +0800

    update

---
+++ fspconfig.js
@@ -0,0 +1,8 @@
+
+{
+    "localPath" : "../css/fontLibary/Canger05w03.ttf",
+    "onlinePath" : "fontLibary/Canger05w03.ttf",
+    "url" :  [
+        "https://nesercode.github.io/index.html"
+    ]
+}        
--- js/myVue.js
+++ js/myVue.js
@@ -19,11 +19,13 @@
 
 const App = {
 	mounted() {
-		this.initApp();
+		this.initApp()
+		this.changeColor()
 	},
 	data() {
 		return {
 			counter: 0,
+			useString: '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~©·—‘’“”…、。《》【】一上不与东个中为么乐也了于云享人什他代们优会但体你使值入全关其再决制前力办加努势北华卷历去受古台同向员咨品哪喜因困国地多大天奔如学宇完官宙定实室寄察将少就山己常平年幸度开式归彩很得心忙快怕总惫想感成我或所才打扫扮据控数断新方无时是智暇暖最有本权李条来果查树欢死每比气没河法波活济浪温滴漫灰点熟爬爱生用由电疲痛百的看真知矾码研砥砺碌社福秀种究空笨算系紫累纳终经绿网群者而联能腾自致色若苦蓝虚蛋行装西要觉解计让讯论访评识词询语请贵赢走路输迎这进远退通道都配野锌问间阅阳难雅青页驭验鱼鼎龄！（），：；？￥',
 			interval: null,
 			bigData: {
 				pickName: '大数据',
@@ -35,8 +37,33 @@ const App = {
 				"品蓝 \n色彩来自 中国色 http://zhongguose.com/",
 				"紫灰 \n色彩来自 中国色 http://zhongguose.com/",
 				"古鼎灰 \n色彩来自 中国色 http://zhongguose.com/",
+				"锌灰 \n色彩来自 中国色 http://zhongguose.com/",
 				"李紫 \n色彩来自 中国色 http://zhongguose.com/",
-				"锌灰 \n色彩来自 中国色 http://zhongguose.com/"
+			],
+			helloColor: [
+				'#de1c31', //唐菖蒲红
+				'#fa7e23', //美人焦橙
+				'#fed71a', //佛手黄
+				'#1ba784', //竹绿
+				'#15559a', //海涛蓝
+				'#22a2c3', //海青
+				'#30161c', //卵石紫
+			],
+			allColors:[
+				'青矾绿 | #2c9678',
+				'品蓝 | #2b73af',
+				'紫灰 | #2b1216',
+				'古鼎灰 | #5d3f51',
+				'李紫 | #36292f',
+				'锌灰 | #7a7374',
+				'唐菖蒲红 | #de1c31',
+				'美人焦橙 | #fa7e23',
+				'佛手黄 | #fed71a',
+				'竹绿 | #1ba784',
+				'海涛蓝 | #15559a',
+				'海青 | #22a2c3',
+				'卵石紫 | #30161c',
+				'色彩来自 中国色 http://zhongguose.com/'
 			],
 			motto: [{
 					text: "没有人能学完所有知识 ，这算是常识",
@@ -89,9 +116,9 @@ const App = {
 					bit: 59,
 				},
 				{
-					text: "忙碌是一种幸福 ，让我们没时间体会痛苦 ;奔波是一种快乐 ，让我们真实地感受生活 ;疲惫是一种享受 ，让我们无暇空虚",
-					step: 53,
-					bit: 101,
+					text: "奔波是一种快乐 ，让我们真实地感受生活 ;疲惫是一种享受 ，让我们无暇空虚",
+					step: 34,
+					bit: 65,
 				},
 				{
 					text: "成熟与年龄无关 ，而是一种阅历 ;优雅与装扮无关 ，而是一种气度",
@@ -132,16 +159,27 @@ const App = {
 				return 1
 			} else return 0
 		},
+		changeColor: function(index) {
+			let target = document.querySelector('span.hello')
+
+			if (index >= 0){
+				target.style.setProperty('--hello-color', this.helloColor[index])
+				return 1;
+			}else return 0;
+		},
 		initApp: function() {
-			let i = 1
+			let i = 1,
+				j = 1
 
 			document.querySelector('.colorinfo').style.width = '1ch'
 			console.log('欢迎访问大数据实验室官网')
-			console.log("查看本页使用的所有色彩，请于控制台输入：\nApp.data().colorfrom.forEach((item)=>{console.log(item)})")
+			console.log("查看本页使用的所有色彩，请于控制台输入：\nApp.data().allColors.forEach((item)=>{console.log(item)})")
 			console.log("查看所有寄语，请于控制台输入：\nApp.data().motto.forEach((item)=>{console.log(item.text)})")
+			console.log("查看本页使用的所有字符，请于控制台输入：\nApp.data().useString")
 
 			this.interval = setInterval(() => {
 				if (this.animateKeyword(i)) i = i == this.motto.length - 1 ? 0 : i + 1
+				if (this.changeColor(j)) j = j == this.helloColor.length - 1 ? 0 : j + 1
 			}, 10000)
 		}
 	}
